439e4f9875b4d71ba8eb7f00a50b15ee10586c08
GodVenn <tlne@equinor.com>
2024-10-08 11:54:37 +0200

    add initial windows setup

---
+++ windows/powershell_profile.ps1
@@ -0,0 +1,7 @@
+# ~\Documents\PowerShell\Microsoft.PowerShell_profile.ps1
+
+$answer = Read-Host "Update all packages? [y/N] "
+if ($answer -ieq "y") {
+	Write-Output "Updating all available packages"
+	winget upgrade --all
+}
---
+++ windows/winget-install-list.ps1
@@ -0,0 +1,24 @@
+winget install -e --id Python.Python.3.12 --silent
+winget install -e --id Git.Git --silent
+winget install -e --id Microsoft.Azure.FunctionsCoreTools --silent
+winget install -e --id Microsoft.Azure.StorageExplorer --silent
+winget install -e --id Microsoft.Azure.StorageEmulator --silent
+winget install -e --id Microsoft.AzureDataStudio --silent
+winget install -e --id Docker.DockerDesktop --silent
+winget install -e --id 7zip.7zip --silent
+winget install -e --id PuTTY.PuTTY --silent
+winget install -e --id Microsoft.PowerToys --silent
+winget install -e --id Notepad++.Notepad++ --silent
+
+winget install -e --id Mozilla.Firefox --silent
+winget install -e --id AgileBits.1Password --silent
+winget install -e --id Microsoft.Office --silent
+winget install -e --id Microsoft.VisualStudioCode --silent
+winget install -e --id Obsidian.Obsidian --silent
+winget install -e --id SlackTechnologies.Slack --silent
+winget install -e --id Discord.Discord --silent
+
+winget install -e --id Spotify.Spotify --silent
+winget install -e --id DuckDuckGo.DesktopBrowser --silent
+
+winget install -e --id kalilinux.kalilinux --silent
